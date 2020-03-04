0ca16e1202bc784b2c9209625daaaa03727f5d8d
ydying <1778679036@qq.com>
2020-03-04 10:09:05 +0800

    修改配置

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,6 +1,26 @@
 import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
-  const config: PowerPartial<EggAppConfig> = {};
-  return config;
+    const config: PowerPartial<EggAppConfig> = {};
+
+    config.cors = {
+        origin: '*',
+        allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+    }
+
+    config.security = {
+        csrf: {
+            enable: false
+        }
+    }
+
+    config.cluster = {
+        listen: {
+            path: '',
+            port: 7020,
+            hostname: '0.0.0.0'
+        }
+    }
+
+    return config;
 };
